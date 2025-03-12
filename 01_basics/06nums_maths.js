5c7f838def9cc5ac00e889f7f33d8dbcf0984873
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-03-12 13:31:32 +0000

    maths

---
+++ 01_basics/06nums_maths.js
@@ -0,0 +1,10 @@
+const score=1234.8999
+
+console.log(score.toFixed(2))
+
+console.log(score.toLocaleString('en-IN'))
+console.log(Math)
+console.log(score.toLocaleString('en-IN'))
+console.log(Math.round(4.567))
+console.log(Math.ceil(4.567))
+console.log(Math.floor(4.567))
