5ce6f5ed4c509a9f61ca8330b8e249a5fc893f92
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-03-21 13:28:01 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+let a=40
+const b=50
+var c=30
+if(true)
+    {
+        var c=100
+        let a=90
+console.log("iiner valye",c)
+console.log("iiner valye",a)
+}
+
+console.log(c)
+
+console.log(a)
