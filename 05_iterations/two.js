530a89abc78ea1a80aa6f778cec65c17155ebcd4
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-03-27 12:02:53 +0000

    while

---
+++ 05_iterations/two.js
@@ -0,0 +1,27 @@
+ //while
+
+  let count=1
+ while(count<=10)
+ {
+    console.log( `value of count is ${count}`)
+    count++
+ }
+
+ //while with array
+
+ let aaraytest=[ "chandan","atharva","juhi"]
+ let index=0
+ while(index<=aaraytest.length)
+    {
+       console.log( `Nme of person is ${aaraytest[index]}`)
+       index++
+    }
+
+    //do while
+     let counttes=10
+
+     do
+     {
+        console.log( `value of counttst is ${counttes}`)
+        counttes++
+     }while(counttes <10)
