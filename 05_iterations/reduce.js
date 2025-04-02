4a10677831b4993d6bbbdc37c7ff12b83df7f3fb
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-04-02 08:20:51 +0000

    reduce on shopping cart

--- 05_iterations/reduce.js
+++ 05_iterations/reduce.js
@@ -4,3 +4,22 @@ const intialvalue=0
 const sum=mynumbers1.reduce((accumulator,current)=>  (accumulator+current),intialvalue)
 
 console.log(sum)
+//shopping car
+const shoppingcart=[
+    {element:"physcics",
+        price:200
+    },
+    {element:"chemistry",
+        price:10000
+    },
+    {element:"math",
+        price:300
+    },
+    {element:"physcics",
+        price:200
+    },
+
+]
+const pricetopay=shoppingcart.reduce((accu,item)=>item.price+accu,0)
+
+console.log(pricetopay)
