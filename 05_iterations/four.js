454d8f7d280e73704eb482a4e9a3b3c48387ccf6
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-04-02 08:12:47 +0000

    reduce

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -32,3 +32,11 @@ const numbers=arr1.filter((item)=>
 
     const newnum1=mynumbers.filter((num)=>num+4)
     console.log(newnum1)
+
+    //chaining of map
+
+    const mynumbers2=[1,2,3,4,5,6,7,8,9]
+
+    const upatenumbers=mynumbers2.map((num)=> num*10)
+                        .map((x)=> x+1)
+                        console.log(upatenumbers)
---
+++ 05_iterations/reduce.js
@@ -0,0 +1,6 @@
+const mynumbers1=[1,2,3]
+const intialvalue=0
+
+const sum=mynumbers1.reduce((accumulator,current)=>  (accumulator+current),intialvalue)
+
+console.log(sum)
