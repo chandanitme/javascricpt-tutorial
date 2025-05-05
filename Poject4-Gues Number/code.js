4ede07e0f420a792b43297597533d2c871ee7a52
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-05-05 09:38:23 +0000

    updated

--- Poject4-Gues Number/code.js
+++ Poject4-Gues Number/code.js
@@ -4,8 +4,50 @@ const userinput=document.querySelector("#guessField")
 const btnSUbmit=document.querySelector("#subt")
 const previousguesscollection=document.querySelector("#idGuesses")
 const guessesremaining=document.querySelector("#idlastreult")
+
+let previousguesses=[]
+let reducegussschane=1
+let playgame=true
+if(playgame)
+{
+    btnSUbmit.addEventListener('click',function(eventdata)
+    {
+        eventdata.preventDefault
+
+        const useinputvalue=parseInt(userinput.value)
+
+        console.log(useinputvalue)
+        validdateuserinpute(useinputvalue)
+
+    },false)
+}
+
+
  function validdateuserinpute(userinputvalue)
  {
+if(isNaN(userinputvalue))
+{
+    alert("Please enter a valid number")
+}
+else if(userinputvalue <1)
+{
+    alert("Please enetr a number greater or equal to 1")
+}
+
+else if(userinputvalue <1)
+    {
+        alert("Please enetr a number less then or equal to 100")
+    }
+    else
+    {
+        previousguesses.push(userinputvalue) 
+        if(reducegussschane<11)
+        {
+            reducegussschane=reducegussschane+1
+        }
+    }
+    
+
 
  }
 
