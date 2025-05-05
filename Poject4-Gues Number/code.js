74dc816b1fc90cbe0455bfdd6837d3373b2e1d00
chandanitme <49269952+chandanitme@users.noreply.github.com>
2025-05-05 08:32:45 +0000

    guess number

---
+++ Poject4-Gues Number/code.js
@@ -0,0 +1,42 @@
+const randomnumber=parseInt(Math.random()*100+1)
+
+const userinput=document.querySelector("#guessField")
+const btnSUbmit=document.querySelector("#subt")
+const previousguesscollection=document.querySelector("#idGuesses")
+const guessesremaining=document.querySelector("#idlastreult")
+ function validdateuserinpute(userinputvalue)
+ {
+
+ }
+
+ function checkGuesses(userinputvalue)
+ {
+
+ }
+
+ function updatecurrentstatus(message)
+ {
+
+ }
+ function displayresultmessage(message)
+ {
+
+ }
+
+ function endgame(message)
+ {
+
+ }
+
+ function startgame(message)
+ {
+
+ }
+
+ 
+
+ 
+
+
+    
+ 
